4404cf5eddfdc262fc35b04f96b2bc0c7db856f8
aybinv7 <aybinventor7@gmail.com>
2023-04-03 15:38:41 +0000

    modifying base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chanti-dz/',
+  base: '/chanti/',
   plugins: [vue()],
   resolve: {
     alias: {
