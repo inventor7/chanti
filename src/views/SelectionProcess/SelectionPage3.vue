b43ef8a79677d8c2277bd0e9e9e01127532c902e
aybinv7 <aybinventor7@gmail.com>
2023-04-05 21:37:53 +0000

    rebuilding emergency page

--- src/views/SelectionProcess/SelectionPage3.vue
+++ src/views/SelectionProcess/SelectionPage3.vue
@@ -1,11 +1,36 @@
 <template>
-    <SignupLayout prevLink="selection-location"   :pageNumber="pNumber" 
-        :isError="notSelectedError" errorText="Please select a service" nextBtnText="Next" @handle="handleClick"
-        pageTitle=" Choose Services" pageDesc=" select the multiple services that you can provide for your clients ">
+    <SignupLayout prevLink="selection-location" :pageNumber="pNumber" :isError="notSelectedError"
+        errorText="Please select a service" nextBtnText="Next" @handle="handleClick" pageTitle=" Choose Services"
+        pageDesc=" select the multiple services that you can provide for your clients ">
 
         <div class=" flex flex-row justify-around w-full h-full flex-1 gap-3  items-start ">
-            <div class=" flex flex-col  justify-start items-start gap-3 w-full h-full ">
-               this is the page 3
+            <div class=" flex flex-col  justify-center items-center gap-3 w-full h-full ">
+                <!-- a select component with modern design to select multiple emergency status as urgent , not urgent , etc -->
+                <transition name="fade">
+                    <div  class="overflow-x-hidden">
+                        <ul tabindex="0"
+                            class="dropdown-content   w-full max-h-32 sm:max-h-64 p-2 sm:shadow-2xl bg-base-100 rounded-box border-y-2 overflow-y-scroll">
+            
+                            <li class=" dropdown-open  px-3 cursor-pointer hover:bg-gray-400/30 font-semibold  py-1 sm:py-2  transition-all duration-200 ease-in-out  rounded-xl   ">
+                                <a class="text-error" > urgent </a>
+                                
+                            </li>
+                            <li class=" dropdown-open  px-3 cursor-pointer hover:bg-gray-400/30 font-semibold  py-1 sm:py-2  transition-all duration-200 ease-in-out  rounded-xl   ">
+                                <a class="text-primary" >in 2 days</a>
+                                
+                            </li>
+                            <li class=" dropdown-open  px-3 cursor-pointer hover:bg-gray-400/30 font-semibold  py-1 sm:py-2  transition-all duration-200 ease-in-out  rounded-xl   ">
+                                <a class="text-warning" >this week </a>
+                                
+                            </li>
+                            <li class=" dropdown-open  px-3 cursor-pointer hover:bg-gray-400/30 font-semibold  py-1 sm:py-2  transition-all duration-200 ease-in-out  rounded-xl   ">
+                                <a class="text-green-700" >this month </a>
+                                
+                            </li>
+                        </ul>
+                    </div>
+                </transition>
+
             </div>
         </div>
 
@@ -43,7 +68,7 @@ export default {
 
 
 
-      
+
 
 
 
