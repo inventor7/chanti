0ccd0acee42dc5793c87998186d7e63df4627a23
aybinv7 <aybinventor7@gmail.com>
2023-05-01 17:39:54 +0100

    cors

---
+++ functions/cors/cors.js
@@ -0,0 +1,11 @@
+exports.handler = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+    body: JSON.stringify({ message: "Hello World" }),
+  };
+  return response;
+};
