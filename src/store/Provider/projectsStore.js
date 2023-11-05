2a849509f56ade68110564ff2ad014f38b4ceb6e
aybinv7 <aybinventor7@gmail.com>
2023-11-05 13:31:09 +0100

    provider projects beta 2

--- src/store/Provider/projectsStore.js
+++ src/store/Provider/projectsStore.js
@@ -37,7 +37,6 @@ export const useProjectsStore = defineStore("projecstStore", {
           timeout: 13000, // 13 seconds
         });
 
-        this.projects = response.data.result;
         this.errorrProvider.status = false;
         this.errorrProvider.message = "";
         this.loading = false;
--- src/views/Provider/Projects.vue
+++ src/views/Provider/Projects.vue
@@ -1,33 +1,46 @@
 <template>
-    <ProviderLayout >
-        <!-- loading and error handling -->
-        <div v-if="loading" class=" flex flex-col justify-center items-center w-full h-full ">
-            <Loading />
-        </div>
+    <ProviderLayout>
 
-        <div v-else
-            class=" flex flex-row w-full justify-between items-center gap-1 rounded-3xl border border-gray-300 my-2 p-1 font-semibold ">
-            <div @click="filterProjects('notStarted')" class=" w-full text-center p-2 rounded-3xl "
+        <!-- Filters -->
+        <div
+            class=" flex flex-row justify-between items-center w-full h-full gap-1 rounded-3xl border border-gray-300 my-2 p-1 font-semibold ">
+            <div @click="filterProjects('notStarted')" class=" cursor-pointer w-full text-center p-2 rounded-3xl "
                 :class="{ 'bg-primary  text-white': Tab === 'notStarted', 'text-black': Tab !== 'notStarted' }">
                 Not started
             </div>
 
-            <div @click="filterProjects('inProgress')" class="  w-full text-center p-2 rounded-3xl "
+            <div @click="filterProjects('inProgress')" class=" cursor-pointer  w-full text-center p-2 rounded-3xl "
                 :class="{ 'bg-primary  text-white': Tab === 'inProgress', 'text-black': Tab !== 'inProgress' }">
                 In progress
             </div>
 
-            <div @click="filterProjects('done')" class="  w-full text-center p-2 rounded-3xl "
+            <div @click="filterProjects('done')" class=" cursor-pointer  w-full text-center p-2 rounded-3xl "
                 :class="{ 'bg-primary  text-white': Tab === 'done', 'text-black': Tab !== 'done' }">
                 Done
             </div>
         </div>
 
-        <div v-if="localProjects.length <= 0" class=" flex flex-col justify-center items-center h-[50vh] ">
-           <img class="w-full" src="../../assets/no-data.svg" alt="">
-           <h2 class=" font-semibold mt-2 " >No Project under this tab</h2>
+
+
+        <!-- loading -->
+        <div v-if="loading" class=" flex flex-col justify-center items-center w-full h-full ">
+            <Loading />
         </div>
-        <Project v-for="project in localProjects " :project="project" :key="project.id" />
+
+        <!-- End Loading -->
+        <div v-else class=" flex flex-col justify-between items-center gap-4 lg:gap-6 w-full h-full">
+
+            <!-- if projects empty -->
+            <div v-if="localProjects.length <= 0" class=" flex flex-col justify-center items-center w-full h-[50vh] ">
+                <img class="w-full" src="../../assets/no-data.svg" alt="">
+                <h2 class=" font-semibold mt-2 ">No Project under this tab</h2>
+            </div>
+
+            <!-- else -->
+            <Project v-for="project in localProjects " :project="project" :key="project.id" />
+
+        </div>
+
     </ProviderLayout>
 </template>
 
@@ -56,17 +69,17 @@ let Tab = ref('notStarted');
 //hooks
 onMounted(() => {
     projectsStore.getProviderProjects(authStore.$state.userAuth.id).then((res) => {
-
+        localProjects.value = notStartedProjects.value
     })
 })
 
-watch(projectsStore.$state.projects , () => {
-    if(projectsStore.$state.projects.notificationClientRequests) {
-    localProjects = [
-        ...projectsStore.$state.projects.notificationClientRequests.map(project => ({ ...project, type: 'clientRequest' })),
-        ...projectsStore.$state.projects.notificationProviderInteresteds.map(project => ({ ...project, type: 'providerInterest' }))
-    ].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-}
+watch(projectsStore.$state.projects, () => {
+    if (projectsStore.$state.projects.notificationClientRequests) {
+        localProjects = [
+            ...projectsStore.$state.projects.notificationClientRequests.map(project => ({ ...project, type: 'clientRequest' })),
+            ...projectsStore.$state.projects.notificationProviderInteresteds.map(project => ({ ...project, type: 'providerInterest' }))
+        ].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+    }
 })
 
 
@@ -83,7 +96,7 @@ let inProgressProjects = computed(() => {
     return localProjects = [
         ...projectsStore.$state.projects.notificationClientRequests.map(project => ({ ...project, type: 'clientRequest' })),
         ...projectsStore.$state.projects.notificationProviderInteresteds.map(project => ({ ...project, type: 'providerInterest' }))
-    ].filter(project => project.status == 'accept').sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+    ].filter(project => project.status == 'accept' || project.status == 'accepted').sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
 })
 
 let doneProjects = computed(() => {
