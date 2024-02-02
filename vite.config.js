35673ead2aa004475d76c38f96f1a1df02de0581
aybinv7 <aybinventor7@gmail.com>
2024-02-02 22:04:19 +0100

    idkkk

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,6 @@ export default defineConfig({
   // include all the assets
   build: {
     assetsInclude: ["assets/**/*"],
-    rollupOptions: {
-      external: ["/wall.svg"],
-    },
   },
   base: "/",
   plugins: [vue()],
