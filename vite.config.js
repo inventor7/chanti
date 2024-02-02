015a41faf8bf86427a131c0ae7ff2c9f568c5ed8
aybinv7 <aybinventor7@gmail.com>
2024-02-02 11:56:50 +0100

    include all assets

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   // include all the assets
-  assetsInclude: ["assets/**/*"],
+  build: {
+    assetsInclude: ["assets/**/*"],
+  },
   base: "/",
   plugins: [vue()],
   resolve: {
